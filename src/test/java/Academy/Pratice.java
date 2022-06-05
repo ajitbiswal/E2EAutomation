455d3f05f66c39299742b685d23d6a8dec0822a9
ajitbiswal <ajit1.biswal@gmail.com>
2022-06-05 19:15:24 +0530

    added

---
+++ src/test/java/Academy/Pratice.java
@@ -0,0 +1,39 @@
+package Academy;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import pageObjects.LandingPage;
+import pageObjects.LoginPage;
+import resources.Base;
+
+//Inheriting the base class
+public class Pratice extends Base{
+	public WebDriver driver;
+	public static Logger log=LogManager.getLogger(Base.class.getName());
+	
+	
+	
+	@Test
+	public void ValidateLogIn(String username,String password) throws IOException {
+		System.out.println("this is first test");
+	}
+	
+	
+	@Test
+	public void ValidateLogIn2(String username,String password) throws IOException {
+		System.out.println("this is second test");
+	}
+	
+	
+	
+	
+
+}
